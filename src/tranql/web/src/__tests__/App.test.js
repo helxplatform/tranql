5960a4450fa7f41a3a08af04eeefb3f2ce004a2d
Griffin Roupe <urnancx@gmail.com>
2021-09-17 13:16:18 -0400

    Replaces domjs with Puppeteer testing
    Replaces domjs jest testing in feature/dependency-updates with end-to-end testing using Puppeteer driver. Adds two temporary tests for prototyping

--- src/tranql/web/src/__tests__/App.test.js
+++ src/tranql/web/src/__tests__/App.test.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { render, screen, getByAttribute } from '@testing-library/react';
-import App from '../App.js';
-import { ForceGraph3D } from 'react-force-graph';
+import puppeteer from 'puppeteer';
+
+let browser;
+let page;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000");
+});
+
+afterAll(async () => {
+    browser.close();
+});
 
 describe('App', () => {
-    it('renders banner', () => {
-        render(<App />);
-        expect(screen.getByText("TranQL")).toBeInTheDocument();
-        expect(screen.getByText("Run")).toBeInTheDocument();
-    });
+    test(
+        "header renders",
+        async () => {
+            await page.waitForSelector("#headerContainer");
+
+            const header = await page.$eval("#headerContainer > p", e => e.innerHTML);
+            expect(header).toEqual("TranQL");
+
+        },
+        30000
+    );
+    test(
+        "schema loads",
+        async () => {
+            await page.waitForSelector(".Legend");
+            await page.waitFor(2000);
+            // todo: Provide dummy graph to React by mocking requests
+            const legendNodes = await page.$eval(".graph-element-type-container > div > div", (el) => el.children.length);
+            expect(legendNodes).toBeGreaterThan(0);            
+        },
+        30000
+    );
 });
